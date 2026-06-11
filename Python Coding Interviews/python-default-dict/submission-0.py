851aacb890895acc152f955d056db74e34cd35c4
Sevan Lewis-Payne <143532100+Sevan1211@users.noreply.github.com>
2026-06-11 09:57:19 -0500

    Add: python-default-dict - submission-0

---
+++ Python Coding Interviews/python-default-dict/submission-0.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+from typing import List, Dict
+
+
+def count_chars(s: str) -> Dict[str, int]:
+    freq = defaultdict(int)
+    for ch in s:
+        freq[ch] += 1
+    
+    return freq
+
+
+def nested_list_to_dict(nums: List[List[int]]) -> Dict[int, List[int]]:
+    hashmap = defaultdict(list)
+
+    for num in nums:
+        key, value = num[0], num[1:]
+        if key not in hashmap:
+            hashmap[key] = []
+
+        hashmap[key].extend(value)
+    return hashmap
+
+
+# do not modify below this line
+print(count_chars("hello"))
+print(count_chars("helloworld"))
+print(count_chars("areallylongstringwhyareyoureadingthishahalol"))
+
+print(nested_list_to_dict([[1, 2, 3], [4, 5, 6], [1, 4]]))
+print(nested_list_to_dict([[1, 2, 3, 4], [4, 5, 6, 7], [1, 4, 5, 6]]))
+print(nested_list_to_dict([[5, 2, 3, 4, 5], [4, 5, 6, 7, 8], [5, 6, 7, 8, 9]]))
+print(nested_list_to_dict([[3, 2, 3, 4, 5], [4, 5, 6, 7, 8], [5, 6, 7, 8]]))
